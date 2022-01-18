547d7b119c5ed29154b4786b944dc7456cb78519
rose <syn@motel.localdomain>
2022-01-18 17:07:46 -0600

    Edit default splash screen in kernel core

--- kernel.c
+++ kernel.c
@@ -5,19 +5,23 @@
 
 #include "vga.h"
 
+#define G_COLOR_PRIMARY WHITE
+#define G_COLOR_SECONDARY CYAN
+#define G_COLOR_TERTIARY BLACK
+
+
 void kernel_entry(void)
 {
-	char *str = "hello, world!";			// str for testing (hardcoded)
+	vga_init(G_COLOR_PRIMARY, CYAN);
 
-	vga_init(WHITE, CYAN);				// clear video memory
-
-	vga_print_string(str, WHITE, CYAN);		// print tests to screen
-	vga_print_newline(BLACK, WHITE);
-	vga_print_char('a', BLACK, WHITE);
-	vga_print_int(40967, BRIGHT_GREEN, RED);
-	vga_print_newline(BLACK, WHITE);
-	vga_print_newline(BLACK, WHITE);
-	vga_print_string(":D", WHITE, BLUE);
+	vga_print_string("tinykernel alpha", G_COLOR_PRIMARY, G_COLOR_SECONDARY);	// print tests to screen
+	vga_print_newline(G_COLOR_PRIMARY, G_COLOR_SECONDARY);
+	
+	unsigned int i;
+	for(i = 0; i < 80; i++)
+	{
+		vga_print_char(223, G_COLOR_PRIMARY, G_COLOR_SECONDARY);	// ASCII code for top half block char
+	}
 
 	return;
 }
